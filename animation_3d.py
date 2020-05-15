3ff52218d5d4fb4c799bc2d727af4e977c4f35b4
torsteoe <47139578+torsteoe@users.noreply.github.com>
2020-05-15 19:17:17 +0200

    add 3d plot of system hanging mass

--- animation_3d.py
+++ animation_3d.py
@@ -1,20 +1,14 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import animation
 import matplotlib.lines as lines
 import numpy as np
 from scipy.integrate import odeint
 
-#rectangle = plt.Rectangle((10,10), 100, 100, fc='r', ec='y')
-#plt.gca().add_patch(rectangle)
-
-
 fig = plt.figure()
-fig.set_dpi(100)
-fig.set_size_inches(7,6.5)
+ax = fig.add_subplot(111, projection='3d')
 
-ax = plt.axes(xlim=(-50,50), ylim=(-15, 20))
-patch = plt.Circle((5, -5), 0.75,fc='b')
-
+x0, y0, z0 = 1,1,1
 def dynamics(q, t):
     y0 = 2
     g = 9.81
@@ -34,6 +28,7 @@ def dynamics(q, t):
     dqdt = [theta_dot, theta_dot_dot, y_dot, y_dot_dot]
     return dqdt
 
+
 def getPos(q):
     theta = q[0]
     theta_dot = q[1]
@@ -42,39 +37,45 @@ def getPos(q):
 
     x_pos = y*np.sin(theta)
     y_pos = -y*np.cos(theta)
-    return (x_pos, y_pos)
+    z_pos = z0
+    return (x_pos, y_pos, z_pos)
 
 theta_init = np.pi/2
 theta_dot_init = 0
 y_init = 4
 y_dot_init = 3
+
+q_init = [theta_init, theta_dot_init, y_init, y_dot_init]
+
 N = 100
 tf = 20
 t = np.linspace(0, tf,N)
-q_init = [theta_init, theta_dot_init, y_init, y_dot_init]
+
 init_pos = getPos(q_init)
 state_traj = odeint(dynamics, q_init, t)
-line, = plt.plot((0,init_pos[0]), (0, init_pos[1]), lw=2, color='black')
+sc = ax.scatter((init_pos[0]), (init_pos[1]), (init_pos[2]), cmap='Greens')
 
 def init():
-    patch.center = (getPos(q_init))
-    ax.add_patch(patch)
-    ax.add_line(line)
+    pass
+    #return sc,
+    
+    #patch.center = (getPos(q_init))
+    #ax.add_patch(patch)
 
-    return line, patch,   
+    #return line, patch,   
 
 
 def animate(i):
     q = state_traj[i]
     pos = getPos(q)
 
-    line.set_xdata((0,pos[0]))
-    line.set_ydata((0, pos[1]))
-    patch.center = pos
-    return line, patch,
+    sc._offsets3d = ([pos[0]], [pos[1]], [pos[2]])
+    print(sc._offsets3d)
+    #return sc,
 
-
-anim = animation.FuncAnimation(fig, animate, init_func=init, 
-                     frames=N, interval=200, blit=True)
-plt.draw
+ax.set_xlim3d(-5, 5)
+ax.set_ylim3d(-10,10)
+ax.set_zlim3d(-2,2)
+anim = animation.FuncAnimation(fig, animate,  
+                     frames=N, interval=200, blit=False)
 plt.show()
